00df99ed8be99e073443e9ae2c37f5def39a8ee3
Baldomero Vela III <74266847+baldo-vela@users.noreply.github.com>
2024-10-23 16:13:52 -0500

    Initialize project & Repo

---
+++ service/src/main/java/com/bvela3/Algo1.java
@@ -0,0 +1,10 @@
+package com.bvela3;
+
+/**
+ * Hello world!
+ */
+public class Algo1 extends App{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
